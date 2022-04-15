9b9abc82622edb3fd0a7fddad69fab5d33793ca7
mechnotech <mechnotech@yandex.ru>
2022-04-15 13:32:31 +0300

    Counter get and put + tests

--- src/service/balancer/counter.py
+++ src/service/balancer/counter.py
@@ -10,7 +10,6 @@ from typing import Optional
 from aioredis import Redis
 
 
-
 class AbstractCounter(abc.ABC):
 
     @abc.abstractmethod
@@ -30,6 +29,39 @@ class RedisCounter(AbstractCounter):
     def __init__(self, redis: Redis):
         self.redis = redis
 
+    async def put(self, server_id: str, film_uuid: str, quality: str) -> bool:
+        await self.redis.incr(name=f'{server_id}.{film_uuid}.{quality}')
+        return True
+
+    async def get_counter(self,
+                          server_id: str,
+                          film_uuid: Optional[str] = None,
+                          quality: Optional[str] = None) -> int:
+
+        async def scan_values(key):
+            cnt = 0
+            async for k in self.redis.scan_iter(match=key + '*'):
+                res = await self.redis.get(name=k)
+                if res:
+                    cnt += int(res)
+            return cnt
+
+        if not film_uuid and quality:
+            return 0
+
+        if film_uuid and not quality:
+            result = await scan_values(f'{server_id}.{film_uuid}')
+
+        elif not film_uuid and not quality:
+            result = await scan_values(f'{server_id}')
+
+        else:
+            result = await self.redis.get(name=f'{server_id}.{film_uuid}.{quality}')
+
+        if result is None:
+            return 0
+        return int(result)
+
     """
     ключ в редисе вида server_id.film_uuid.quality
 
@@ -37,7 +69,7 @@ class RedisCounter(AbstractCounter):
     отдать управление
 
     get_counter показывает запросы по серверу если остальное None
-                            запросы по фильму независимо от качества
-                            запросы по фильму с конкретным качеством
-                            спросить про качество без фильма нельзя
+                            запросы по фильму независимо от качества +
+                            запросы по фильму с конкретным качеством +
+                            спросить про качество без фильма нельзя +
     """
--- src/tests/test_counter.py
+++ src/tests/test_counter.py
@@ -1,7 +1,10 @@
+import uuid
+
 import pytest
 import pytest_asyncio
 import aioredis
 
+from service.balancer.counter import RedisCounter
 from service.config import config
 
 
@@ -13,6 +16,7 @@ async def redis_connect():
 
     connection = await aioredis.from_url(REDIS_LINK)
     yield connection
+    await connection.flushall(asynchronous=True)
     await connection.close()
 
 
@@ -20,3 +24,97 @@ async def redis_connect():
 async def test_example(redis_connect):
     """ """
     assert await redis_connect.ping()
+
+
+@pytest.mark.asyncio
+async def test_simple_put(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    key = f'{server_id}.{film_uuid}.{quality}'
+    value = 1
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    result = await redis_connect.get(name=key)
+    assert int(result) == value
+
+
+@pytest.mark.asyncio
+async def test_put_return_bool(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    result = await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    assert isinstance(result, bool)
+
+
+@pytest.mark.asyncio
+async def test_simple_get(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    value = 1
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    result = await counter.get_counter(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    assert result == value
+
+
+@pytest.mark.asyncio
+async def test_get_nonexistent_key(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    result = await counter.get_counter(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    assert result == 0
+
+
+@pytest.mark.asyncio
+async def test_get_none(redis_connect):
+    counter = RedisCounter(redis_connect)
+    result = await counter.get_counter(server_id=None)
+    assert result == 0
+
+
+@pytest.mark.asyncio
+async def test_value_update(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    key = f'{server_id}.{film_uuid}.{quality}'
+    value = 2
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    result = await redis_connect.get(name=key)
+    assert int(result) == value
+
+
+@pytest.mark.asyncio
+async def test_get_without_quality(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    quality = '720'
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    result = await counter.get_counter(server_id=server_id, film_uuid=film_uuid)
+    assert result == 2
+
+
+@pytest.mark.asyncio
+async def test_get_sever_stats_only(redis_connect):
+    counter = RedisCounter(redis_connect)
+    server_id = str(uuid.uuid4())
+    film_uuid = str(uuid.uuid4())
+    quality = '360'
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    film_uuid = str(uuid.uuid4())
+    quality = '1080'
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    await counter.put(server_id=server_id, film_uuid=film_uuid, quality=quality)
+    result = await counter.get_counter(server_id=server_id)
+    assert result == 3
