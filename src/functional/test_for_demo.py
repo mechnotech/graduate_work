1d82d752e22b9314cb2d73b9f205da7bb9e61c3f
althusky <althusky@gmail.com>
2022-04-23 11:45:49 +0000

    wip 04-23-3

--- src/functional/test_for_demo.py
+++ src/functional/test_for_demo.py
@@ -3,9 +3,18 @@ Only for presentation
 """
 import logging
 
+import requests
 import pytest
 
+URL = "http://localhost:8080/api/v1/link/{uuid}/{quality}?user_ip={ip}"
 
-@pytest.mark.skip
-def test_demo():
+
+def test_demo(data_set):
+    film_uuid = "22e7c14e-8c47-4155-aafc-a123d45fd357"
+    quality = '360'
+    user_ip = '192.168.1.117'
+    response = requests.get(URL.format(uuid=film_uuid,
+                                       quality=quality,
+                                       ip=user_ip))
+    print(response)
     logging.info("Демо")
